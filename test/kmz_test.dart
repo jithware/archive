8982059bd8f72771c0bf58ef91b467d75e5ea469
jithware <jithware@gmail.com>
2025-08-16 13:47:56 -0400

    #239: Update tests

--- test/kmz_test.dart
+++ test/kmz_test.dart
@@ -7,11 +7,44 @@ import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 void main() async {
-  group('kmz', () {
-    String kmzDataDir = 'test/_data/kmz';
-    List<String> kmzTestFiles = ['test.kmz', 'test2.kmz'];
+  final kmzDataDir = 'test/_data/kmz';
+  final kmzOutDir = '$kmzDataDir/_out';
+  List<String> kmzTestFiles = ['test.kmz', 'test2.kmz'];
+  List<String> kmzOutFiles = [];
 
-    test('decode and encode file', () async {
+  setUpAll(() async {
+    if (await Directory('$kmzOutDir').exists()) {
+      await Directory('$kmzOutDir').delete(recursive: true);
+    }
+    // extract all test files
+    for (int i = 0; i < kmzTestFiles.length; i++) {
+      final kmzTestDir =
+          "$kmzOutDir/${p.basenameWithoutExtension(kmzTestFiles[i])}";
+      final archive = ZipDecoder().decodeBytes(
+          File('$kmzDataDir/${kmzTestFiles[i]}').readAsBytesSync());
+
+      // extract files and directories from kmz
+      for (final file in archive) {
+        final filename = file.name;
+        if (file.isFile) {
+          final data = file.content as List<int>;
+          File('$kmzTestDir/' + filename)
+            ..createSync(recursive: true)
+            ..writeAsBytesSync(data);
+        } else {
+          await Directory('$kmzTestDir/' + filename).create(recursive: true);
+        }
+      }
+    }
+  });
+
+  tearDownAll(() async {
+    print(
+        'Create a map at: https://www.google.com/maps/d/ and test importing $kmzOutFiles.');
+  });
+
+  group('kmz', () {
+    test('can be encoded and decoded', () async {
       final archive = ZipDecoder().decodeStream(InputMemoryStream(
           File('$kmzDataDir/${kmzTestFiles[0]}').readAsBytesSync()));
 
@@ -22,52 +55,57 @@ void main() async {
       expect(archive.length, archive2.length);
     });
 
-    test('extract and recreate same file', () async {
-      final kmzOutDir = '$kmzDataDir/_out';
-      if (await Directory('$kmzOutDir').exists()) {
-        await Directory('$kmzOutDir').delete(recursive: true);
-      }
-
+    test('can recreate same file', () async {
       for (int i = 0; i < kmzTestFiles.length; i++) {
-        final kmzOutFile = '$kmzOutDir/${kmzTestFiles[i]}';
+        final newKmzFile = '$kmzOutDir/${kmzTestFiles[i]}';
         final kmzTestDir =
             "$kmzOutDir/${p.basenameWithoutExtension(kmzTestFiles[i])}";
 
-        final kmzBytes =
-            File('$kmzDataDir/${kmzTestFiles[i]}').readAsBytesSync();
-        final archive = ZipDecoder().decodeBytes(kmzBytes);
-
-        // extract files from kmz
-        for (final file in archive) {
-          final filename = file.name;
-          if (file.isFile) {
-            final data = file.content as List<int>;
-            File('$kmzTestDir/' + filename)
-              ..createSync(recursive: true)
-              ..writeAsBytesSync(data);
-          } else {
-            await Directory('$kmzTestDir/' + filename).create(recursive: true);
-          }
-        }
+        final originalArchive = ZipDecoder().decodeBytes(
+            File('$kmzDataDir/${kmzTestFiles[i]}').readAsBytesSync());
 
         // zip extracted directory
         var encoder = ZipFileEncoder();
         await encoder.zipDirectory(Directory('$kmzTestDir'),
-            filename: '$kmzOutFile');
+            filename: '$newKmzFile');
 
         // decode the archive we just encoded
-        final outBytes = File('$kmzOutFile').readAsBytesSync();
-        final archive2 = ZipDecoder().decodeBytes(outBytes, verify: true);
+        final newArchive = ZipDecoder()
+            .decodeBytes(File('$newKmzFile').readAsBytesSync(), verify: true);
 
-        expect(archive2.length, equals(archive.length));
-        for (var i = 0; i < archive2.length; ++i) {
-          expect(archive2[i].name, equals(archive[i].name));
-          expect(archive2[i].size, equals(archive[i].size));
+        expect(newArchive.length, equals(originalArchive.length));
+        for (var i = 0; i < newArchive.length; ++i) {
+          expect(newArchive[i].name, equals(originalArchive[i].name));
+          expect(newArchive[i].size, equals(originalArchive[i].size));
         }
 
-        print(
-            'Create a map at: https://www.google.com/maps/d/ and import $kmzOutFile.');
+        kmzOutFiles.add(newKmzFile);
       }
+    });
+
+    test('can recreate same file with no directories', () async {
+      final newKmzFile = '$kmzOutDir/test3.kmz';
+      final kmzTestDir =
+          "$kmzOutDir/${p.basenameWithoutExtension(kmzTestFiles[0])}";
+
+      final originalArchive = ZipDecoder().decodeBytes(
+          File('$kmzDataDir/${kmzTestFiles[0]}').readAsBytesSync());
+
+      // zip extracted directory without directories
+      var encoder = ZipFileEncoder();
+      await encoder.zipDirectory(Directory('$kmzTestDir'),
+          filename: '$newKmzFile',
+          filter: (entity, _) => entity is Directory
+              ? ZipFileOperation.skip
+              : ZipFileOperation.include);
+
+      // decode the archive we just encoded
+      final newArchive = ZipDecoder()
+          .decodeBytes(File('$newKmzFile').readAsBytesSync(), verify: true);
+
+      expect(newArchive.length, equals(originalArchive.length - 1));
+
+      kmzOutFiles.add(newKmzFile);
     });
   });
 }
